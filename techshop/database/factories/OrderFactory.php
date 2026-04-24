0f289d049ab855b2ed9da6803e1e0773f3f8a8a1
Mac <mac@MacBook-Pro-4.local>
2026-04-24 18:42:09 +0200

    feat: add factories and seeders for database models

---
+++ techshop/database/factories/OrderFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Order;
+use App\Models\User;
+use App\Enums\OrderStatus;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Order>
+ */
+class OrderFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'total_price' => fake()->randomFloat(2, 10, 2000),
+            'status' => fake()->randomElement(array_column(OrderStatus::cases(), 'value')),
+        ];
+    }
+}
